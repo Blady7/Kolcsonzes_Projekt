e999e587157d3f8579d1643d3925a57a20f1aa91
Blady7 <petihuawei2@gmail.com>
2025-03-17 15:00:10 +0100

    request kész

--- Backend/database/SQL.sql
+++ Backend/database/SQL.sql
@@ -3,5 +3,13 @@
 	COLLATE utf8_hungarian_ci;
 
 Select * from books;
+Select * from users;
+Select * from groups;
+Select * from rentals;
+Select * from readingdiaries;
+Select * from roles;
+Select * from specimens;
+
+
 
 
