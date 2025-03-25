c69d8d4565ac57ff2f6ce9ccc3a1d7a4ec96287f
Blady7 <petihuawei2@gmail.com>
2025-03-25 09:16:07 +0100

    ja

--- Frontend/src/views/KonyvView.vue
+++ Frontend/src/views/KonyvView.vue
@@ -1,13 +1,40 @@
 <template>
-  
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Költő</th>
+          <th scope="col">Cím</th>
+          <th scope="col">Évfolyam</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
