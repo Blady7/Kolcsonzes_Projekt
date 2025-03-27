e03f03bd2e8cab166c337968e153c2aeb84a4e69
BigFeri <ferenc0516@gmail.com>
2025-03-27 11:16:51 +0100

    DiakView/DiakForm elkezédse

---
+++ Frontend/src/components/DiakForm.vue
@@ -0,0 +1,53 @@
+<template>
+    <form
+      @submit.prevent="onClickSubmit"
+      class="row g-3 needs-validation was-validated"
+    >
+      <p v-if="debug">{{ itemForm }}</p>
+      <div class="col-md-4 position-relative">
+        <label for="tittle" class="form-label">Név:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="tittle"
+          required
+          v-model="itemForm.tittle"
+        />
+      </div>
+  
+      <div class="col-md-2 position-relative">
+        <label for="poet" class="form-label">Osztály:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="poet"
+          required
+          v-model="itemForm.poet"
+        />
+      </div>
+  
+      <div class="col-md-2 position-relative">
+        <label for="groupId" class="form-label">Email:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="groupId"
+          required
+          v-model="itemForm.groupId"
+        />
+      </div>
+      <button type="submit" class="btn btn-success">Mentés</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    props: ["itemForm", "items", "debug"],
+    emits: ["saveItem"],
+    methods: {
+      onClickSubmit() {
+        this.$emit("saveItem", this.itemForm);
+      },
+    },
+  };
+  </script>
--- Frontend/src/views/DiakView.vue
+++ Frontend/src/views/DiakView.vue
@@ -1,13 +1,212 @@
 <template>
+    <div>
+      <table class="my-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Név</th>
+            <th>Osztály</th>
+            <th>Email</th>
+            <th>Műveletek</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, index) in paginatedItems" :key="index">
+            <td>{{ user.id }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.groupId }}</td>
+            <td>{{ user.email }}</td>
+            <td class="text-nowrap text-center">
+              <OperationsCrud
+                @onClickDeleteButton="onClickDeleteButton(user)"
+                @onClickUpdate="onClickUpdate(user)"
+                @onClickCreate="onClickCreate"
+                :data="user"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Paginator
+        :totalItems="items.length"
+        :itemsPerPage="20"
+        :currentPage="currentPage"
+        @page-changed="updatePage"
+      />
+      <Modal
+        :title="title"
+        :yes="yes"
+        :no="no"
+        :size="size"
+        @yesEvent="yesEventHandler"
+      />
+    </div>
+  </template>
   
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+  <script>
+  import Paginator from "@/components/Paginator.vue";
+  import axios from "axios";
+  import { BASE_URL } from "../helpers/baseUrls";
+  import DiakForm from "@/components/DiakForm.vue";
+  import ErrorMessage from "@/components/ErrorMessage.vue";
+  
+  class Item {
+    constructor(poet = null, title = null, groupId = null) {
+      this.poet = poet;
+      this.title = title;
+      this.groupId = groupId;
+    }
+  }
+  
+  export default {
+    components: { Paginator, DiakForm, ErrorMessage },
+    data() {
+      return {
+        items: [],
+        currentPage: 1,
+        itemsPerPage: 20,
+        item: new Item(),
+        messageYesNo: null,
+        state: "Read",
+        title: null,
+        yes: null,
+        no: null,
+        size: null,
+        errorMessages: null,
+        modal: null,
+        selectedRowId: null,
+        token: localStorage.getItem("token") || "", // Token betöltése localStorage-ból
+      };
+    },
+    computed: {
+      paginatedItems() {
+        const start = (this.currentPage - 1) * this.itemsPerPage;
+        return this.items.slice(start, start + this.itemsPerPage);
+      },
+    },
+    async mounted() {
+      await this.getItems();
+    },
+    methods: {
+      async getItems() {
+        try {
+          const response = await axios.get(`${BASE_URL}/users`, {
+            headers: { Authorization: `Bearer ${this.token}` },
+          });
+          this.items = response.data.data;
+        } catch (error) {
+          console.error("Hiba történt az adatok lekérése közben:", error);
+        }
+      },
+  
+      updatePage(page) {
+        this.currentPage = page;
+      },
+  
+      async deleteItemById() {
+        if (!this.selectedRowId) return;
+        try {
+          await axios.delete(`${BASE_URL}/users/${this.selectedRowId}`, {
+            headers: { Authorization: `Bearer ${this.token}` },
+          });
+          this.getItems();
+        } catch (error) {
+          this.errorMessages = "A könyv nem törölhető";
+        }
+      },
+  
+      async createItem() {
+        try {
+          await axios.post(`${BASE_URL}/users`, this.item, {
+            headers: { Authorization: `Bearer ${this.token}` },
+          });
+          this.getItems();
+        } catch (error) {
+          this.errorMessages = "A bővítés nem sikerült.";
+        }
+        this.state = "Read";
+      },
+  
+      async updateItem() {
+        if (!this.selectedRowId) return;
+        try {
+          await axios.patch(`${BASE_URL}/users/${this.selectedRowId}`, this.item, {
+            headers: { Authorization: `Bearer ${this.token}` },
+          });
+          this.getItems();
+        } catch (error) {
+          this.errorMessages = "A módosítás nem sikerült.";
+        }
+        this.state = "Read";
+      },
+  
+      yesEventHandler() {
+        if (this.state === "Delete") {
+          this.deleteItemById();
+        }
+      },
+  
+      onClickDeleteButton(item) {
+        this.state = "Delete";
+        this.selectedRowId = item.id;
+        this.title = "Törlés";
+        this.messageYesNo = `Valóban törölni akarod a(z) ${item.title} nevű könyvet?`;
+        this.yes = "Igen";
+        this.no = "Nem";
+      },
+  
+      onClickUpdate(item) {
+        this.state = "Update";
+        this.selectedRowId = item.id;
+        this.item = { ...item };
+        this.title = "Könyv módosítása";
+        this.no = "Mégsem";
+        this.size = "lg";
+      },
+  
+      onClickCreate() {
+        this.state = "Create";
+        this.item = new Item();
+        this.title = "Új könyv bevitele";
+        this.no = "Mégsem";
+        this.size = "lg";
+      },
+  
+      onClickTr(id) {
+        this.selectedRowId = id;
+      },
+  
+      onClickCloseErrorMessage() {
+        this.errorMessages = null;
+        this.state = "Read";
+      },
+  
+      saveItemHandler() {
+        if (this.state === "Update") {
+          this.updateItem();
+        } else if (this.state === "Create") {
+          this.createItem();
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .container {
+    margin-top: 40px;
+    text-align: center;
+  }
+  
+  th {
+    background-color: #c19a6b;
+    color: white;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+  
+  td {
+    color: #4a3b2d;
+  }
+  </style>
+  
