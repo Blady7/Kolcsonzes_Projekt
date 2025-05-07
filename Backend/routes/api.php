bf39b54285c0198436fd752605427a160b24963c
Blady7 <petihuawei2@gmail.com>
2025-05-07 11:21:50 +0200

    api

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -22,15 +22,15 @@ Route::get('queryDiakValaszto/{limit}/{offset}', [QueriesController::class, 'que
 Route::get('queryTanarnevsor', [QueriesController::class, 'queryTanarnevsor']);
 Route::get('queryStudentsCount', [QueriesController::class, 'queryStudentsCount']);
 Route::get('users', [UserController::class, 'index'])
-    ->middleware('auth:sanctum');
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::get('users/{id}', [UserController::class, 'show'])
-    ->middleware('auth:sanctum');
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::post('users', [UserController::class, 'store'])
-    ->middleware('auth:sanctum');
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::patch('users/{id}', [UserController::class, 'update'])
-    ->middleware('auth:sanctum');
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::delete('users/{id}', [UserController::class, 'destroy'])
-    ->middleware('auth:sanctum');
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 //endregion
 
 //region books
@@ -39,67 +39,56 @@ Route::get('books/{id}', [BookController::class, 'show']);
 Route::get('queryOsztalyAzon/{limit}/{offset}', [QueriesController::class, 'queryOsztalyAzon']);
 Route::get('queryBooksCount', [QueriesController::class, 'queryBooksCount']);
 Route::post('books', [BookController::class, 'store'])
-    ->middleware('auth:sanctum');
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::patch('books/{id}', [BookController::class, 'update'])
-    ->middleware('auth:sanctum');
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::delete('books/{id}', [BookController::class, 'destroy'])
-    ->middleware('auth:sanctum');
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 //endregion
 
 //region groups
 Route::get('groups', [GroupController::class, 'index']);
 Route::get('queryTanarValaszto', [QueriesController::class, 'show']);
 Route::post('groups', [GroupController::class, 'store'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::patch('groups/{id}', [GroupController::class, 'update'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::delete('groups/{id}', [GroupController::class, 'destroy'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 //endregion
 
 //region rentals
-Route::get('rentals', [RentalController::class, 'index']);
+Route::get('rentals', [RentalController::class, 'index'])
+    ->middleware('auth:sanctum', CheckAbilities::class.':rentals:view');
 Route::get('queryKolcsonzesAzon/{limit}/{offset}', [ QueriesController::class, 'queryKolcsonzesAzon']);
 Route::get('queryRentalsCount', [QueriesController::class, 'queryRentalsCount']);
 Route::get('rentals/{id}', [RentalController::class, 'show']);
 Route::post('rentals', [RentalController::class, 'store'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::patch('rentals/{id}', [RentalController::class, 'update'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::delete('rentals/{id}', [RentalController::class, 'destroy'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 //endregion
 
 //region roles
 Route::get('roles', [RoleController::class, 'index']);
 Route::get('roles/{id}', [RoleController::class, 'show']);
 Route::post('roles', [RoleController::class, 'store'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::patch('roles/{id}', [RoleController::class, 'update'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::delete('roles/{id}', [RoleController::class, 'destroy'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 //endregion
 
 //region specimens
 Route::get('specimens', [SpecimenController::class, 'index']);
 Route::get('specimens/{id}', [SpecimenController::class, 'show']);
 Route::post('specimens', [SpecimenController::class, 'store'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::patch('specimens/{id}', [SpecimenController::class, 'update'])
-    ->middleware('auth:sanctum');
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');
 Route::delete('specimens/{id}', [SpecimenController::class, 'destroy'])
-    ->middleware('auth:sanctum');    
-
+    ->middleware('auth:sanctum', CheckAbilities::class.':*');    
 //endregion
